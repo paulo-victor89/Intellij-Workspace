33ff34137de482a0d38bee67edb1f53e4d865d28
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-25 15:23:01 -0300

    Corrigindo pontos

--- src/pvictor/poo/encapsulamento/Aula06.java
+++ src/pvictor/poo/encapsulamento/Aula06.java
@@ -4,10 +4,11 @@ public class Aula06 {
 
     public static void main(String[] args) {
         ControleRemoto c = new ControleRemoto();
-        //c.ligar();
+        c.ligar();
         c.maisVolume();
+        c.menosVolume();
         c.play();
-        //c.ligarMudo();
+        c.ligarMudo();
         c.abrirMenu();
         c.fecharMenu();
 
--- src/pvictor/poo/encapsulamento/ControleRemoto.java
+++ src/pvictor/poo/encapsulamento/ControleRemoto.java
@@ -11,7 +11,7 @@ public class ControleRemoto implements Controlador {
     // Constructor
     public ControleRemoto() {
         this.volume = 50;
-        this.ligado = true;
+        this.ligado = false;
         this.tocando = false;
     }
 
@@ -57,6 +57,7 @@ public class ControleRemoto implements Controlador {
         /*
             Implementar um if para so mostrar o menu se estiver ligado
          */
+        if(this.getLigado()){
         System.out.println("-----Menu-----");
         System.out.println("Está ligado? " + this.getLigado());
         System.out.println("Está tocando? " + this.getTocando());
@@ -65,19 +66,28 @@ public class ControleRemoto implements Controlador {
             System.out.print(" | ");
         }
         System.out.println("");
+    }else if (!(this.getLigado())){
+            System.out.println("Erro: O controle está desligado!");
+        }
     }
 
     @Override
     public void fecharMenu() {
-        System.out.println("Fechando Menu...");
+        //System.out.println("Fechando Menu...");
+        if(!(this.getLigado())){
+            System.out.println("Erro: o controle está desligado");
+        }
+        else if(!(this.getLigado())){
+            System.out.println("Fechando Menu...");
+        }
     }
 
     @Override
     public void maisVolume() {
         if (this.getLigado()) {
             this.setVolume(this.getVolume() + 5);
-        }else{
-            System.out.println("Impossivel aumentar volume");
+        }else if (!(this.getLigado())){
+            System.out.println("Erro: O controle está desligado");
         }
     }
 
@@ -85,8 +95,8 @@ public class ControleRemoto implements Controlador {
     public void menosVolume() {
         if (this.getLigado()) {
             this.setVolume(this.getVolume() - 5);
-        }else{
-            System.out.println("Impossivel diminuir volume!");
+        }else if(!(this.getLigado())){
+            System.out.println("Erro: O controle está desligado");
         }
 
     }
@@ -95,7 +105,11 @@ public class ControleRemoto implements Controlador {
     public void ligarMudo() {
         if (this.getLigado() && this.getVolume() > 0) {
             this.setVolume(0);
-
+            //this.setTocando(false);
+        }else if(this.getLigado()&& this.getVolume()==0){
+            System.out.println("Erro: O volume já está no mudo");
+        }else if(!(this.getLigado())){
+            System.out.println("Erro: O controle está desligado");
         }
 
     }
@@ -104,28 +118,40 @@ public class ControleRemoto implements Controlador {
     public void desligarMudo() {
         if (this.getLigado() && this.getVolume() == 0) {
             this.setVolume(50);
+        }else if (this.getVolume()>0 && this.getLigado()){
+            System.out.println("Aviso: O volume não está no mudo");
+        }else if(!(this.getLigado())){
+            System.out.println("Erro: O controle está desligado");
         }
 
     }
 
     @Override
-    public void play() {
-        if (this.getLigado() && !(this.getTocando())) {
+    public void play(){
+        if(this.getLigado() && !(this.getTocando())){
             this.setTocando(true);
-        }else{
-            System.out.println("Não consegui reproduzir");
         }
-
+        else if(this.getLigado() && this.getTocando()){
+            System.out.println("Aviso: o sistema já está no modo play");
+        }
+        else if(!(this.getLigado())){
+            System.out.println("Erro: o controle está desligado");
+        }
     }
 
-    @Override
-    public void pause() {
-        if (this.getLigado() && this.getTocando()) {
-            this.setTocando(false);
-        }else{
-            System.out.println("Não consegui pausar");
-        }
 
+    @Override
+
+    public void pause(){
+        if(this.getLigado() && this.getTocando()){
+            this.setTocando(false);
+        }
+        else if(this.getLigado() && !(this.getTocando())){
+            System.out.println("Aviso: o sistema já está no modo pause");
+        }
+        else if(!(this.getLigado())){
+            System.out.println("Erro: o controle está desligado");
+        }
     }
 
 

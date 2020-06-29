fbc94ff063aaf2100891866056bf3a3ad46051f1
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-29 16:17:27 -0300

    Adicionando o projeto livro

---
+++ src/pvictor/poo/ProjetoLivro/ProjetoLivro.java
@@ -0,0 +1,24 @@
+package pvictor.poo.ProjetoLivro;
+
+public class ProjetoLivro {
+    public static void main(String[] args) {
+        Pessoa [] p = new Pessoa[2];
+        Livro [] l = new Livro[3];
+
+        p[0] = new Pessoa("Pedro", 22, "M");
+        p[1] = new Pessoa("Maria",28, "F");
+
+        l[0] = new Livro("Aprendendo java","José da Silva", 300,p[0]);
+        l[1] = new Livro("POO para Iniciantes","Pedro Paulo",500, p[1]);
+        l[2] = new Livro("Java Avancado","Maria Candido",800,p[0]);
+
+        l[0].abir();
+        l[0].folhear(299);
+        l[0].avancarPag();
+        System.out.println(l[0].detalhes());
+
+        System.out.println(l[1].detalhes());
+
+    }
+
+}
---
+++ src/pvictor/poo/ProjetoLivro/Publicacao.java
@@ -0,0 +1,11 @@
+package pvictor.poo.ProjetoLivro;
+
+public interface Publicacao {
+    public void abir();
+    public void fechar();
+    public void folhear(int p);
+    public void avancarPag();
+    public void voltarPag();
+
+
+}
